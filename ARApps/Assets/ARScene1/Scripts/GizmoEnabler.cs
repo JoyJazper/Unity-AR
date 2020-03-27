0bf8deee42aae179e53827be791d7b0ee847eeed
P Joy Kumar <pjk096@gmail.com>
2020-03-27 16:54:21 +0530

    Rotational Gizmo Implememnted. Version 0.5 Dev build released.

---
+++ ARApps/Assets/ARScene1/Scripts/GizmoEnabler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoEnabler : MonoBehaviour
+{
+    public bool gizmoEnabled = false;
+    public GameObject gizmoPrefab;
+    // Start is called before the first frame update
+
+    public void EnableGizmo()
+    {
+        if(gizmoEnabled)
+        {
+            gizmoEnabled = false;
+            gizmoPrefab.gameObject.SetActive(true);
+        }
+        else
+        {
+            gizmoEnabled = true;
+            gizmoPrefab.gameObject.SetActive(false);
+        }
+    }
+}
